d5e56690942d40d8d4538eba181b8fb740c9b4a4
gurmindersingh751 <30909302+gurmindersingh751@users.noreply.github.com>
2017-10-21 14:29:27 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var un = document.loginform.usr.value;
+    var pw = document.loginform.pword.value;
+    var username = "admin";
+    var password = "admin";
+    if ((un == username) && (pw == password)) {
+        window.location = "some.html";
+        return true;
+    } else {
+        alert("Login was unsuccessful, please check your username and password");
+        return false;
+    }
